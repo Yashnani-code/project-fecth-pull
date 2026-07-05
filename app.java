b7afa3618ef17e6f92d463433b1727ca8d321406
yashwanth <yashnani0512@gmail.com>
2026-07-05 07:49:08 +0000

    sub

--- app.java
+++ app.java
@@ -1,7 +1,8 @@
 public class app{
 	public static void main(string[] args){
 		int a=100, int= 20;
-		system.out.println("The sum is;"(a+b));
+		system.out.println("The sum is;"+(a+b));
+		system.out.println("The sub is:"+(a-b));
 	}
 }
 
