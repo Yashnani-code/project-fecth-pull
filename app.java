8fee3de653a9d7f80d96523f528f4dbc4302d36c
yaswanth kumar chimaladinne <yashnani0512@gmail.com>
2026-07-08 15:39:49 +0530

    Fix variable declaration syntax in app.java

--- app.java
+++ app.java
@@ -1,6 +1,7 @@
 public class app{
 	public static void main(string[] args){
-		int a=100, int b= 20;
+		int a=100;
+		int b= 20;
 		system.out.println("The sum is;"+(a+b));
 		system.out.println("The sub is:"+(a-b));
 		system.out.println("The mul is:"+(a*b));
