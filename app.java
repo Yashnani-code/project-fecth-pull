294f9f5bde91ae2fa98742854a15e7c5e74f6adf
yaswanth kumar chimaladinne <yashnani0512@gmail.com>
2026-07-08 15:33:00 +0530

    Fix variable declaration for 'b' in app.java

--- app.java
+++ app.java
@@ -1,6 +1,6 @@
 public class app{
 	public static void main(string[] args){
-		int a=100, int= 20;
+		int a=100, int b= 20;
 		system.out.println("The sum is;"+(a+b));
 		system.out.println("The sub is:"+(a-b));
 		system.out.println("The mul is:"+(a*b));
