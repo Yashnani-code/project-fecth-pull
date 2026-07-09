90daf197713fc2f19c845420c89394d0f78d34f7
yaswanth kumar chimaladinne <yashnani0512@gmail.com>
2026-07-09 09:45:30 +0530

    Fix casing and syntax in app.java

--- app.java
+++ app.java
@@ -1,11 +1,11 @@
 public class app{
-	public static void main(string[] args){
+	public static void main(String[] args){
 		int a=100;
 		int b= 20;
-		system.out.println("The sum is;"+(a+b));
-		system.out.println("The sub is:"+(a-b));
-		system.out.println("The mul is:"+(a*b));
-		system.out.println("The div is:"+(a/b));
+		System.out.println("The sum is;"+(a+b));
+		System.out.println("The sub is:"+(a-b));
+		System.out.println("The mul is:"+(a*b));
+		System.out.println("The div is:"+(a/b));
 	}
 }
 
